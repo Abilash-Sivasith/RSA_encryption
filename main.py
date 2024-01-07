e313b0444d36bcd1fe37aa543be4b57f6b69ff70
Abilash Sivasith <abi.sivasith.world@gmail.com>
2024-01-07 17:55:28 +1300

    added ALPHABET_MAPPING dict

--- main.py
+++ main.py
@@ -2,7 +2,34 @@
 
 import math
 import random
-
+ALPABET_MAPPING = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7,
+    'i': 8,
+    'j': 9,
+    'k': 10,
+    'l': 11,
+    'm': 12,
+    'n': 13,
+    'o': 14,
+    'p': 15,
+    'q': 16,
+    'r': 17,
+    's': 18,
+    't': 19,
+    'u': 20,
+    'v': 21,
+    'w': 22,
+    'x': 23,
+    'y': 24,
+    'z': 25
+}
 def prime_checker(number):
     '''returns true if the number passed through is a prime'''
     sqrt_of_num = int(math.sqrt(number))
@@ -78,8 +105,6 @@ def RSA_encryption(plain_text, p , q):
             list_of_letters_to_encode.append(letter)
     for letter in list_of_letters_to_encode:
         pass
-
-
 
 
 def RSA_decryption():
